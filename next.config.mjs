0e7db1fcd6e3bc644cf4c7e3cabf9803d2cee5dd
florian foerster <florian.foerster@hotmail.de>
2025-11-03 11:09:19 +0100

    Remove unoptimized images setting from next.config.mjs and update multiple image files

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,6 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  images: {
-    unoptimized: true,
-  },
   experimental: {
     webpackBuildWorker: true,
     parallelServerBuildTraces: true,
